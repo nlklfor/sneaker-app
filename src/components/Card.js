130834b45b03ae500f0f494f83ad616f20c7f587
nikifor <nik.slepcov1771@gmail.com>
2023-03-30 17:37:28 +0300

    state added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 
 function Card(props) {
-    const added = () => {
-        alert("You succesfully added " + props.name + " to your cart");
+    const [checked, setChecked] = useState(false)
+    
+    const unChecked = () => {
+        setChecked(!checked);   {/*! означает что переменная может быть инвертирована т.е при нажатии менять своё внутреннее значение в противоположном направлении*/}
+    }
+    // const onAddCart = () => {
+    //     alert("You succesfully added " + props.name + " to your cart");
+    // }
+    const onAddFav = () => {
+        alert("You succesfully added " + props.name + " to your favourite list");
     }
     return (
         <div className='card'>
-            <div className='favourite'>
-                <img src='/img/heart-unactive.svg' alt='Unactive' />
-            </div>
+            <img className="favourite" onClick={onAddFav} src='/img/heart-unactive.svg' alt='Unactive' />
             <img src={props.img} alt='Nike' width={133} height={112} />
             <p className='card_desc'>
                 Men shoes <br />
@@ -20,9 +27,7 @@ function Card(props) {
                         {props.price}
                     </span>
                 </div>
-                <button className='card_button' type="submit" onClick={() => added()}>
-                    <img alt="Add" src='/img/plus.svg'></img>
-                </button>
+                <img className="btn_add" onClick={unChecked} alt="Add" src={checked ? "/img/checked.svg" : "/img/plus.svg"}></img>  {/*  ? = IF TRUE...   : = IF FALSE... */}
             </div>
         </div>
     )
